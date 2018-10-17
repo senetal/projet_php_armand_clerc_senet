5905fca0ad0ed00a89ed4b640b8eb5d5e9acf145
SENET Alexandre <senetal@hostname.iut2.upmf-grenoble.fr>
2018-10-17 16:52:55 +0200

    database created

--- project/Modele/create.sql
+++ project/Modele/create.sql
@@ -1,13 +1,15 @@
-create table category()
+create table category(
   name varchar(32),
-  category varchar(32) reference category(name)
+  category varchar(32),
+  foreign key(category) references category(name)
 );
 
-create table products()
+create table products(
   ref integer primary key,
   name varchar(64) not null,
   price float default 0.00,
   image varchar(16),
   description varchar(256),
-  category varchar(32) reference category(name)
+  category varchar(32),
+  foreign key(category) references category(name)
 );

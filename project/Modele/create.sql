378c899fc9dae7294b4a2dbd6e6af832a7b8d38a
CLERC Max <clercma@hostname.iut2.upmf-grenoble.fr>
2018-10-18 10:13:12 +0200

    'master' of https://github.com/senetal/projet_php_armand_clerc_senet

--- project/Modele/create.sql
+++ project/Modele/create.sql
@@ -1,5 +1,5 @@
 create table category(
-  name varchar(32),
+  name varchar(32) primary key,
   category varchar(32),
   foreign key(category) references category(name)
 );
@@ -12,4 +12,11 @@ create table products(
   description varchar(256),
   category varchar(32),
   foreign key(category) references category(name)
+);
+create table user(
+
+  id integer primary key,
+  name varchar(64) not null,
+  password varchar(64) not null,
+  mail varchar(64) not null
 );

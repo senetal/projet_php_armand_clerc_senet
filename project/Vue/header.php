d313f7847c84a43c121679b5652c8c5656d77646
ARMAND Sylvain <armansyl@hostname.iut2.upmf-grenoble.fr>
2018-10-19 11:34:03 +0200

    header

---
+++ project/Vue/header.php
@@ -0,0 +1,25 @@
+<?php
+function header(){
+  echo
+  <div class="accueil">
+    <nav>
+      <a title="Acceuil" href=""></a>
+    </nav>
+  </div>
+  <div class="">
+    <nav>
+      <ul>
+        <a title="Dépot d'annonce" href=""></a>
+        <a title="Offres" href=""></a>
+        <a title="Demandes" href=""></a>
+      </ul>
+    </nav>
+  </div>
+  <div class="connexion">
+    <nav>
+      <a title="Connexion" href=""></a>
+    </nav>
+  </div>;
+}
+
+?>
--- project/Vue/main.view.php
+++ project/Vue/main.view.php
@@ -1,3 +1,5 @@
+<?php include 'header.php'; ?>
+
 <!DOCTYPE html>
 <html lang=fr dir="ltr">
   <head>
@@ -5,7 +7,20 @@
     <title>Accueil</title>
   </head>
   <body>
+    <header>
+      <?php header(); ?>
+    </header>
     <h1>Accueil</h1>
     <p>pour plus de contenu il faut attendre la fin du projet</p>
+
+
+
+    <div class="">
+
+    </div>
+
+    <footer>
+      <?php  ?>
+    </footer>
   </body>
 </html>

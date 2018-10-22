9921736f9e0e005115a01ce3a585224332d1987a
ARMAND Sylvain <armansyl@hostname.iut2.upmf-grenoble.fr>
2018-10-22 16:20:36 +0200

    option connexion

--- project/Vue/header.php
+++ project/Vue/header.php
@@ -8,7 +8,12 @@
         <li><a href="../Controleur/Annonces.ctrl.php">Dépot d'annonce</a></li>
         <li><a href="../Controleur/Products.ctrl.php">Offres</a></li>
         <li><a href="../Controleur/Demandes.ctrl.php">Demandes</a></li>
+        <?php $name = 'toto'; ?>
+        <?php if (isset($name)): ?>
+        <li style="float:right"><div class="refConnexion"><a href="../Controleur/account.ctrl.php"><?php echo $name; ?></a></div></li>
+        <?php else: ?>
         <li style="float:right"><div class="refConnexion"><a href="../Controleur/account.ctrl.php">Connexion</a></div></li>
+        <?php endif ?>
       </ul>
     </div>
   </nav>

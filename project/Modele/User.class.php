69bc8c80a0bc711448e7755eac9ca69b4120d96b
CLERC Max <clercma@hostname.iut2.upmf-grenoble.fr>
2018-10-22 09:11:10 +0200

    change pour fixe la classe user

--- project/Modele/User.class.php
+++ project/Modele/User.class.php
@@ -1,5 +1,5 @@
-<?php
 
+<?php
 class User{
 public $name;
 public $password;
@@ -7,13 +7,13 @@ public $mail;
 public $tel;
 public $address;
 
-  function __construct(string $name,string $password,string $mail,string $tel, string $address){}
+  function __construct(string $name,string $password,string $mail,string $tel, string $address){
     $this->name=$name;
     $this->password=$password;
     $this->mail=$mail;
     $this->tel=$tel;
     $this->address=$address;
   }
+}
 
-
- ?>
+?>

7163d03963a4d05e54b5eff4e556f82818a01a32
senetal <39625053+senetal@users.noreply.github.com>
2018-10-22 08:32:24 +0200

    Constructeur user

--- project/Modele/User.class.php
+++ project/Modele/User.class.php
@@ -8,9 +8,14 @@ public $mail;
 public $tel;
 public $address;
 
-  function __construct(){}
-
-}
+  function __construct(int $id,string $name,string $password,string $mail,string $tel, string $address){}
+    $this->id=$id;
+    $this->name=$name;
+    $this->password=$password;
+    $this->mail=$mail;
+    $this->tel=$tel;
+    $this->address=$address;
+  }
 
 
  ?>

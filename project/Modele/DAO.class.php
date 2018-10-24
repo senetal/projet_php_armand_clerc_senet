1a3e9a46fbfb5f1d2788f0b745615a9fd98b48b8
SENET Alexandre <senetal@hostname.iut2.upmf-grenoble.fr>
2018-10-24 15:36:08 +0200

    bug fix (creates more bugs)

--- project/Modele/DAO.class.php
+++ project/Modele/DAO.class.php
@@ -134,8 +134,8 @@ var_dump($tab);
 }
 
 function getUser(string $name, string $password):User{
-	$req="select * from user where name=':name' and password=':password'";
-	$query = ($this->db)->prepare($req);
+	$req="SELECT * from user where name='$name' and password='$password'";
+	$query = ($this->db)->query($req);
 	$tab = $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE,'User');
 return $tab[0];
 }
